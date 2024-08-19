c3d50d1f3ee90efec60a9ca69220ed5bc21f486b
viniciussqr <124948842+viniciussqr@users.noreply.github.com>
2024-08-19 16:47:28 -0400

    Add files via upload
    commit código atualizado

--- GabineteMedico/app/src/main/java/com/example/gabinetemedico/Splash.java
+++ GabineteMedico/app/src/main/java/com/example/gabinetemedico/Splash.java
@@ -4,24 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicial);
+        setContentView(R.layout.activity_splash);
 
         Button buttonLogin = findViewById(R.id.buttonLogin);
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Splash.this, Login.class);
-                startActivity(intent);
+        buttonLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(Splash.this, Login.class);
+            startActivity(intent);
 
-            }
         });
         }
     }
